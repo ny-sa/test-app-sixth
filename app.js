40d9a4d4cd5ad9074797e9d13c776aa902160b64
nysa <kusogedump@gmail.com>
2020-02-13 15:18:35 +0100

    in progress

--- app.js
+++ app.js
@@ -1,13 +1,67 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
- 
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const app = express();
  
-app.use(express.static("public"));
-app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
  
-app.listen(3000, function() {
-    console.log("Server started on port 3000.");
+mongoose.connect('mongodb://localhost:27017/userDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+
+const User = new mongoose.model('User', userSchema);
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(err => {
+            if (!err) res.render('secrets');
+        });
+    });
+});
+ 
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email: username}, (err, foundUser) => {
+        if (!err)
+            if (foundUser)
+                bcrypt.compare(password, foundUser.password, (err, result) => {
+                    if(result) res.render('secrets');
+                });
+    });
+});
+
+app.listen(2020, () => {
+    console.log('Server started on port 2020.');
 });
